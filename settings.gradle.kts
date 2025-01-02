fa4fd6a0353555eab697cb19a884f6b34388726f
mikooomich <mikooomichael@gmail.com>
2025-01-02 18:06:12 -0500

    gradle: Add local extractor override

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,16 @@ include(":innertube")
 include(":kugou")
 include(":lrclib")
 include(":material-color-utilities")
+
+
+
+// Use a local copy of NewPipe Extractor by uncommenting the lines below.
+// We assume, that NewPipe and NewPipe Extractor have the same parent directory.
+// If this is not the case, please change the path in includeBuild().
+
+//includeBuild("../TubularExtractor") {
+//    dependencySubstitution {
+//        substitute(module("com.github.gechoto:NewPipeExtractor"))
+//            .using(project(":extractor"))
+//    }
+//}
