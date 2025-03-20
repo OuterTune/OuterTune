f2d54172d7926bb6b6c80db75b769a2500567c8d
mikooomich <mikooomichael@gmail.com>
2025-03-20 00:06:57 -0400

    player: Try resumption (properly) AGAIN

--- app/src/main/java/com/dd3boh/outertune/MainActivity.kt
+++ app/src/main/java/com/dd3boh/outertune/MainActivity.kt
@@ -149,7 +149,6 @@ import com.dd3boh.outertune.constants.SlimNavBarKey
 import com.dd3boh.outertune.constants.StopMusicOnTaskClearKey
 import com.dd3boh.outertune.db.MusicDatabase
 import com.dd3boh.outertune.db.entities.SearchHistory
-import com.dd3boh.outertune.extensions.toEnum
 import com.dd3boh.outertune.playback.DownloadUtil
 import com.dd3boh.outertune.playback.MusicService
 import com.dd3boh.outertune.playback.MusicService.MusicBinder
