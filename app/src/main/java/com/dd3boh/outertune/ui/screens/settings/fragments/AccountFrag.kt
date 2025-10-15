c2d7b92dd2bd398cb275749bed1515e63b44f55b
RA-L-PH <ralphaacarvalho@gmail.com>
2025-10-15 17:38:59 +0530

    fix: Add missing fillMaxWidth import in AccountFrag.kt

--- app/src/main/java/com/dd3boh/outertune/ui/screens/settings/fragments/AccountFrag.kt
+++ app/src/main/java/com/dd3boh/outertune/ui/screens/settings/fragments/AccountFrag.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.Logout
 import androidx.compose.material.icons.rounded.Key
