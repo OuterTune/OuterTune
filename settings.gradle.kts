ac93245a7ca0ea2588f51a2aa5260d703e94b1a7
Anass Elgmairi <112984613+Golarvo@users.noreply.github.com>
2026-05-04 17:07:20 +0100

    Add plugin management repositories
    Added plugin management repositories to settings.gradle.kts.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,15 @@
 @file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-
     repositories {
         google()
         mavenCentral()
