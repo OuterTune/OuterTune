f24c3beff815c7ed54ba120af04a598269fa3936
Anass Elgmairi <112984613+Golarvo@users.noreply.github.com>
2026-05-04 17:17:42 +0100

    Reorganize plugin declarations in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,11 @@
 plugins {
-    alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.ksp) apply false
-}
-
-kotlin {
-    jvmToolchain(17)
-}
-
-dependencies {
-    implementation(libs.ktor.client.core)
-    implementation(libs.ktor.client.okhttp)
-    implementation(libs.ktor.client.content.negotiation)
-    implementation(libs.ktor.serialization.json)
-    implementation(libs.ktor.client.encoding)
-    implementation(libs.brotli)
-    implementation("com.github.teamnewpipe:NewPipeExtractor")
-    testImplementation(libs.junit)
+    alias(libs.plugins.aboutlibraries) apply false
 }
