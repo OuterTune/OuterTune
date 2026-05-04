a4d665be1c1ba447f73630c18ff6de6a98d8221e
Anass Elgmairi <112984613+Golarvo@users.noreply.github.com>
2026-05-04 16:55:12 +0100

    Update plugin application settings in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,12 @@
 plugins {
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.kotlin.ksp) apply false
 }
 
 kotlin {
