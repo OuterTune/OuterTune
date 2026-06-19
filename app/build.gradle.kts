cf119586cc0a5a4e26a42375d180731d9cd63bbd
yuuichi <lov.music.jaz@gmail.com>
2026-06-19 20:30:33 +0900

    chore: bump version to 0.10.5 (versionCode 75)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.dd3boh.outertune"
         minSdk = 24
         targetSdk = 36
-        versionCode = 74
-        versionName = "0.10.4"
+        versionCode = 75
+        versionName = "0.10.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
